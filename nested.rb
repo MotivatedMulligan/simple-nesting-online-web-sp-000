e8d83d7a96a04dddc2e2fcef831283f2195d867e
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-21 17:28:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		programmer_hash.fetch(:dennis_ritchie).fetch(:languages){|v|v.to_s}
+		programmer_hash.fetch(:dennis_ritchie).fetch(:languages).to_str
 end
 
 def changing_alan
